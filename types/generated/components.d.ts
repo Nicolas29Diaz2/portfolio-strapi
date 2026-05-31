6c68a87ecfe2f82e92205b6b9649c5784bbeb3af
Nicolás Díaz <diazsantosnicolas10@gmail.com>
2026-05-31 04:22:23 -0500

    feat(products): products page collection

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,3 +1,34 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedFooter extends Struct.ComponentSchema {
+  collectionName: 'components_shared_footers';
+  info: {
+    displayName: 'Footer';
+    icon: 'bulletList';
+  };
+  attributes: {
+    linkItem: Schema.Attribute.Component<'shared.link-item', true>;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedLinkItem extends Struct.ComponentSchema {
+  collectionName: 'components_shared_link_items';
+  info: {
+    displayName: 'linkItem';
+    icon: 'bulletList';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.footer': SharedFooter;
+      'shared.link-item': SharedLinkItem;
+    }
+  }
+}
